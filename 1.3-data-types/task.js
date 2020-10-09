f7d1dc5186d33a703411a44a738bd2b65f171dda
Andrey-1966 <64395108+Andrey-1966@users.noreply.github.com>
2020-10-09 17:24:51 +0400

    Add files via upload

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -9,41 +9,56 @@ function rdns(a,k) {
 function monthDiffrent(data1, data2) {
     let months;
     months = (data2.getFullYear() - data1.getFullYear()) * 12;
-    months -= data1.getMonth() + 1; //данный месяц не считаем
+    months -= data1.getMonth(); //данный месяц не считаем
     months += data2.getMonth();
     return months <= 0 ? 0 : months;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    
+    let Total = '';
+    let counts = true;
     //провекрка на "вшивость" введенных данных
-    if (typeof percent != "number") {
+    if (!isNaN(percent)) {
         percent = Number.parseInt(percent);
-    }
-   if (typeof contribution != "number") {
-        contribution = Number.parseInt(contribution);
-    }
-    if (typeof amount != "number") {
-        amount = Number.parseInt(amount);
-    } 
+    } else {
+    	Total = `Параметр percent содержит неправильное значение ` + `${percent}`;
+    	counts = false;
+	}
 
-    let P = (percent / 12) /100;
-    let now = new Date(); //Сегодняшняя дата. 
-    let n = monthDiffrent(now, date); //подсчет месяцев между датами. Текущий месяц не считается!!!
-    let S = amount - contribution; //Тело кредита
-    
-    let Total = S * (P + P / ( Math.pow((1 + P),n)-1));
+   if (!isNaN(contribution)) {
+        contribution = Number.parseInt(contribution);
+    } else {
+    	Total += `Параметр contribution содержит неправильное значение ` + `${contribution}`;
+    	counts = false;
+	}
+
+    if (!isNaN(amount)) {
+        amount = Number.parseInt(amount);
+    } else {
+    	Total += `Параметр amount содержит неправильное значение ` + `${amount}`;
+    	counts = false;
+	}
+
+	if (counts) {
+	    let P = (percent / 12) /100;
+	    let now = new Date(); //Сегодняшняя дата. 
+	    let n = monthDiffrent(now, date); //подсчет месяцев между датами. Текущий месяц не считается!!!
+	    let S = amount - contribution; //Тело кредита
+	    
+	    let Total = n * S * (P + P / ( Math.pow((1 + P),n)-1));
+	    return rdns(Total,2);
+	} else return Total;
     //Формула, данная в задании (вот она, сверху) не работает.
     //Если решить задачу с равномерным уменьшением остатком по кредиту
     //ежемесячно, то вот рабочая формула:
 
    // let Total = S *(1 + (n + 1) / 2 * P / 100);
     // return "Total = " + rdns(Total,2) + ": за " + n + " месяцев (сегодняшний месяц не учитывается!)";
-    return rdns(Total,2);
+    
 }
 
 function getGreeting(name) {
-     // код для задачи №2 писать здесь
+     let hello = `Привет, мир! Меня зовут `;
      if ((!name) || (name.trim().length == 0)) return `Привет, мир! Меня зовут Аноним`;
      else  return `Привет, мир! Меня зовут ${name}`;
 }
