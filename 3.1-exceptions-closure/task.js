13e9c334e3f36681350030771ec380f7d6856540
Andrey-1966 <64395108+Andrey-1966@users.noreply.github.com>
2020-10-25 14:35:49 +0400

    Add files via upload

---
+++ 3.1-exceptions-closure/task.js
@@ -0,0 +1,48 @@
+function parseCount(str) {
+if (isNaN(str)) throw new Error('Недопустимое значение');
+return Number.parseInt(str);
+}
+
+function validateCount(str) {
+try {
+	parseCount(str);
+} catch {
+	return false;
+}
+return parseCount(str);
+}
+
+//Задача 2
+class Triangle {
+	constructor(a,b,c) {
+		if ((a+b <= c) || (a+c <= b) || (b+c <= a)) {
+			throw new Error('Треугольник с такими сторонами не существует');
+		}
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	getArea() {
+		let p = this.getPerimeter() /2;
+		return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+	}
+
+}
+
+//const tri = new Triangle(3,4,5);
+
+function getTriangle(a,b,c) {
+	try {
+      tris = new Triangle(a,b,c);
+	} catch(e) {
+      throw new Error('Ошибка! Треугольник не существует');
+	}
+	return tris;
+}
+
+//console.log(getTriangle(3,4,5));
